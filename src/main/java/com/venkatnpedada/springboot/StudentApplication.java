2b6aa630579cad1a7dd66d491aaaec885376429c
venkatnpedada <venkatnpedada@gmail.com>
2018-05-04 16:36:28 +0530

    spring boot REST services boilerplate code

---
+++ src/main/java/com/venkatnpedada/springboot/StudentApplication.java
@@ -0,0 +1,16 @@
+package com.venkatnpedada.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+
+@SpringBootApplication
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(
+				StudentApplication.class, args);
+		//System.out.println(ctx);
+	}
+}
